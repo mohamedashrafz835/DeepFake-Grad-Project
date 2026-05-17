9095ce5a5a54705b9dc42e6f9fea18a1687b1dc2
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2026-05-17 07:48:40 +0300

    Update main.tf for terrafrom backend

--- bootstarp/main.tf
+++ bootstarp/main.tf
@@ -5,14 +5,46 @@ provider "aws" {
 # -------------------------------
 # Remote Backend Setup
 # -------------------------------
-resource "aws_s3_bucket" "state-bucket" {
+
+resource "aws_s3_bucket" "state_bucket" {
   bucket = "statefile-bucket-2210xz"
+
   tags = {
-    Name = "state-buckethawarey"
+    Name = "state-bucket"
   }
 }
 
-resource "aws_dynamodb_table" "statefile-table" {
+# Enable versioning (IMPORTANT)
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Encrypt state file
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block public access
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "statefile_table" {
   name         = "statefile-table"
   hash_key     = "LockID"
   billing_mode = "PAY_PER_REQUEST"
